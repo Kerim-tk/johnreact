c103463f3aadf43610de62331fa0a185e7d3cbb3
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-17 14:05:59 +0400

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
 import './App.scss';
+import { NavBar } from './NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <NavBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/NavBar.tsx
@@ -0,0 +1,12 @@
+import './NavBar.scss';
+
+export const NavBar = () => {
+    return (
+        <nav>
+        <a href="#">Home</a>
+        <a href="#">Catalog</a>
+        <a href="#">About</a>
+        </nav>
+
+    );
+};
