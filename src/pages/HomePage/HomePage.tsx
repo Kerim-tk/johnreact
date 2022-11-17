0b5d9213e359bb7493977e76d7c0c83f40d9169f
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-17 15:19:23 +0400

    5

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,32 @@
-import './HomePage.scss';
+import "./HomePage.scss";
+
 
 export const HomePage = () => {
-    return (
-        <h1>home</h1>
-    );
-}
+  return (
+    <div className="hero-block">
+      <div className="narrow-container">
+        <h1 className="hero-h1">
+          Hi, I&nbsp;am John,
+          <br />
+          Creative Technologist
+        </h1>
+        <p className="hero-p">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor
+          do&nbsp;amet sint. Velit officia consequat duis enim velit mollit.
+          Exercitation veniam consequat sunt nostrud amet.
+        </p>
+        <button className="hero-button">
+          <p className="hero-button-p">Download Resume</p>
+        </button>
+      </div>
+      <div className="container">
+        <img
+          className="hero-img-shadow"
+          src="./Ellipse 2.png"
+          alt="shadow"
+        />
+        <img className="hero-img" src="./Ellipse 1.png" alt="avatar" />
+      </div>
+    </div>
+  );
+};
