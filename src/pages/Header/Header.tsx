06950e8f669cc736a24128c51ecce5ed557bec1a
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-17 15:04:07 +0400

    header done

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -2,6 +2,12 @@ import './Header.scss';
 
 export const Header = () => {
     return (
-        <header>header</header>
+        <div>
+        <header className="header desktop-only">
+          <a href="work.html">Works</a>
+          <a href="#">Blog</a>
+          <a href="#">Contact</a>
+        </header>
+      </div>
     );
 }
