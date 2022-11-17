592a5719644e73d4632585447554e198ed90d330
Kerim <86295374+Kerim-tk@users.noreply.github.com>
2022-11-17 14:56:14 +0400

    добавил хеадер маин и футер

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './assets/images/logo.svg';
-import './App.scss';
-import { NavBar } from './NavBar';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./App.scss";
+import { NavBar } from "./NavBar";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { Header } from "./pages/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <NavBar />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <main>
+        <HomePage />
+      </main>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
     </div>
   );
 }
