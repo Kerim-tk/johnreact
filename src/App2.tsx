12a5fd2bea9c5b8734d5eb60d56e0e07d4c574b7
Kerim-tk <alekperovkerim@gmail.com>
2022-11-21 09:36:35 +0400

    1

---
+++ src/App2.tsx
@@ -0,0 +1,16 @@
+import { Header } from "./pages/Header/Header";
+import { HomePage } from "./pages/HomePage/HomePage";
+
+function App() {
+    return (
+      <div className="App">
+        <Header/>
+        <main>
+          
+        </main>
+        <footer>
+          <h2>Footer</h2>
+        </footer>
+      </div>
+    );
+  }
--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,10 +1,12 @@
 import './Header.scss';
+import '../../App2'
+
 
 export const Header = () => {
     return (
         <div>
         <header className="header desktop-only">
-          <a href="work.html">Works</a>
+          <a href="app2.tsx">Works</a>
           <a href="#">Blog</a>
           <a href="#">Contact</a>
         </header>
